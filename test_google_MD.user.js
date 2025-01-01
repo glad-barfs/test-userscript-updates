06c2ff2d40a7142c8f8fad6bcc316e94fcafa0de
asdfblarg <david82794@yahoo.com>
2025-01-01 02:34:15 -0500

    update url paths and version

--- test_google_MD.user.js
+++ test_google_MD.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         Test change google image to MD
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      3.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.google.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
-// @updateURL    https://github.com/glad-barfs/test-userscript-updates/raw/main/test_google_MD.user.js
-// @downloadURL  https://github.com/glad-barfs/test-userscript-updates/raw/main/test_google_MD.user.js
+// @updateURL    https://github.com/glad-barfs/test-userscript-updates/raw/main/src/test_google_MD.user.js
+// @downloadURL  https://github.com/glad-barfs/test-userscript-updates/raw/main/src/test_google_MD.user.js
 // @grant        none
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    document.querySelector("#hplogo").src = "https://mangadex.org/img/brand/mangadex-wordmark.svg"
+    document.querySelector("#hplogo").src = "https://mangadex.org/img/brand/mangadex-logo.svg"
 })();
