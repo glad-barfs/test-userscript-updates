c2ff6f3e90ff6fc25b9638cd718a3814fdda8b15
asdfblarg <david82794@yahoo.com>
2025-01-01 02:23:23 -0500

    add urls

--- test_google_MD.user.js
+++ test_google_MD.user.js
@@ -6,10 +6,12 @@
 // @author       You
 // @match        https://www.google.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @updateURL    https://github.com/glad-barfs/test-userscript-updates/raw/main/test_google_MD.user.js
+// @downloadURL  https://github.com/glad-barfs/test-userscript-updates/raw/main/test_google_MD.user.js
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     document.querySelector("#hplogo").src = "https://mangadex.org/img/brand/mangadex-logo.svg"
